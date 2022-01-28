b93a3298ea81e66c98189fceff57b7c6de7f3013
Legenda759 <oripov1994@gmail.com>
2022-01-28 23:45:13 +0500

    Solutions to PDP Online Tasks

--- Task_5_2_3/ContentView.swift
+++ Task_5_2_3/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var first = ""
+    @State var last = ""
+    @State var email = ""
+    @State var phone = ""
+    @State var sms = ""
+    @State var address1 = ""
+    @State var address2 = ""
+    @State var zipcode = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ScrollView{
+                Group{
+                    Text("First Name").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("First Name", text: $first).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+                Group{
+                    Text("Last Name").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("Last Name", text: $last).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+                Group{
+                    Text("Email").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("Email", text: $email).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+                Group{
+                    Text("SMS").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("SMS", text: $sms).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+                Group{
+                    Text("Address 1").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("Address 1", text: $address1).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+                Group{
+                    Text("Address 2").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("Address 2", text: $address2).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+                Group{
+                    Text("Zipcode").frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    TextField("Zipcode", text: $zipcode).textFieldStyle(RoundedBorderTextFieldStyle()).padding(.horizontal)
+                }
+            }
+        }
     }
 }
 
